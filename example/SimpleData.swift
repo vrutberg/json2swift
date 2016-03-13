199ccf3811401673c1419fd067da0cb35c79acb4
Viktor Rutberg <wishie@gmail.com>
2016-03-13 21:03:14 +0100

    Added indented examples.

--- example/SimpleData.swift
+++ example/SimpleData.swift
@@ -1,10 +1,11 @@
 struct SimpleData {
-let version: String
-let buildNumber: Float
-let description: String
-init(version: String, buildNumber: Float, description: String) {
-self.version = version
-self.buildNumber = buildNumber
-self.description = description
-}
+	let version: String
+	let buildNumber: Float
+	let description: String
+
+	init(version: String, buildNumber: Float, description: String) {
+			self.version = version
+			self.buildNumber = buildNumber
+			self.description = description
+	}
 }
--- example/SimpleDataTestBuilder.swift
+++ example/SimpleDataTestBuilder.swift
@@ -1,31 +1,34 @@
-
 class SimpleDataTestBuilder {
-private var version: String
-private var buildNumber: Float
-private var description: String
-private init() {
-version = "0.0.1"
-buildNumber = 48
-description = "Some simple data format"
-}
- class func aBuilder() -> SimpleDataTestBuilder {
- return SimpleDataTestBuilder()
-}
-func withVersion(version: String) -> SimpleDataTestBuilder {
-self.version = version
-return self
-}
+	private var version: String
+	private var buildNumber: Float
+	private var description: String
 
-func withBuildNumber(buildNumber: Float) -> SimpleDataTestBuilder {
-self.buildNumber = buildNumber
-return self
-}
+	private init() {
+		version = "0.0.1"
+		buildNumber = 48
+		description = "Some simple data format"
+	}
 
-func withDescription(description: String) -> SimpleDataTestBuilder {
-self.description = description
-return self
-}
-func build() -> SimpleData {
-return SimpleData(version: version, buildNumber: buildNumber, description: description)
-}
+	class func aBuilder() -> SimpleDataTestBuilder {
+		return SimpleDataTestBuilder()
+	}
+
+	func withVersion(version: String) -> SimpleDataTestBuilder {
+		self.version = version
+		return self
+	}
+
+	func withBuildNumber(buildNumber: Float) -> SimpleDataTestBuilder {
+		self.buildNumber = buildNumber
+		return self
+	}
+
+	func withDescription(description: String) -> SimpleDataTestBuilder {
+		self.description = description
+		return self
+	}
+
+	func build() -> SimpleData {
+		return SimpleData(version: version, buildNumber: buildNumber, description: description)
+	}
 }
