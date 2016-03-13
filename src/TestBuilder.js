ab1a80d016e2d3f4064e6dfee40afceb9a29363d
Viktor Rutberg <wishie@gmail.com>
2016-03-13 17:32:44 +0100

    Added more functionality to TestBuilder.

--- src/TestBuilder.js
+++ src/TestBuilder.js
@@ -1,6 +1,12 @@
-var TestBuilder = function(struct) {
+var _ = require('lodash');
+
+var TestBuilder = function(struct, defaultValues) {
   if (typeof struct === 'undefined') {
     throw { 'message': 'Cannot be instantiated without a Struct as parameter' };
+  }
+
+  if (typeof defaultValues === 'undefined') {
+    defaultValues = [];
   }
 
   var getName = function() {
@@ -11,9 +17,68 @@ var TestBuilder = function(struct) {
     return "\nclass " + getName() + " {";
   };
 
+  var getPropertyList = function() {
+    var result = "";
+
+    struct.getProperties().forEach(function(property) {
+      result += "\nprivate var "+ property.name + ": " + property.type;
+    });
+
+    return result;
+  };
+
+  var getInitializer = function() {
+    var result = "\nprivate init() {";
+
+    struct.getProperties().forEach(function(property) {
+      result += "\n" + property.name + " = " + defaultValues[property.name];
+    });
+
+    result += "\n}";
+    return result;
+  };
+
+  var getABuilderMethod = function() {
+    return "\n class func aBuilder() -> " + getName() + " {" +
+     "\n return " + getName() + "()" +
+     "\n}"
+  };
+
+  var getSinglePropertySetter = function(property) {
+    return "\nfunc with" + _.upperFirst(property.name) + "(" + property.name + ": " + property.type + ") -> " + getName() + " {" +
+      "\nself." + property.name + " = " + property.name +
+      "\nreturn self" +
+      "\n}"
+  };
+
+  var getPropertySetters = function() {
+    return struct.getProperties().map(getSinglePropertySetter).join("\n");
+  };
+
+  var getBuildMethod = function() {
+    var parameterList = "";
+
+    struct.getProperties().forEach(function(property) {
+      if (parameterList !== "") {
+        parameterList += ", ";
+      }
+
+      parameterList += property.name + ": "+ property.name;
+    });
+
+    return "\nfunc build() -> " + struct.getName() + " {" +
+      "\nreturn " + struct.getName() + "(" + parameterList + ")" +
+      "\n}"
+  };
+
   return {
     toString: function() {
       return getDeclaration() +
+        getPropertyList() +
+        getInitializer() +
+        getABuilderMethod() +
+        getPropertySetters() +
+        getBuildMethod() +
         "\n}";
     }
   };
--- test/TestBuilder.test.js
+++ test/TestBuilder.test.js
@@ -19,5 +19,74 @@ describe('TestBuilder', function() {
 
       expect(testBuilder.toString()).toContain('class AStructTestBuilder {');
     });
+
+    it('should include properties from the struct as properties', function() {
+      var struct = Struct();
+      struct.setName('AStruct');
+      struct.setProperties([
+        { name: 'color', type: 'UIColor' }
+      ]);
+
+      var testBuilder = TestBuilder(struct, []);
+
+      expect(testBuilder.toString()).toContain('private var color: UIColor');
+    });
+
+    it('should have a private initializer', function() {
+      var struct = Struct();
+      struct.setName('AStruct');
+
+      var testBuilder = TestBuilder(struct);
+
+      expect(testBuilder.toString()).toContain('private init() {');
+    });
+
+    it('should make assignments to default values in initializer', function() {
+      var struct = Struct();
+      struct.setName('AStruct');
+      struct.setProperties([
+        { name: 'color', type: 'String' }
+      ]);
+
+      var testBuilder = TestBuilder(struct, { color: '"red"' });
+
+      expect(testBuilder.toString()).toContain('color = "red"');
+    });
+
+    it('should include the aBuilder method', function() {
+      var struct = Struct();
+      struct.setName('AStruct');
+
+      var testBuilder = TestBuilder(struct);
+
+      expect(testBuilder.toString()).toContain('class func aBuilder() -> AStructTestBuilder {');
+    });
+
+    it('should include property setter for a property', function() {
+      var struct = Struct();
+      struct.setName('AStruct');
+      struct.setProperties([
+        { name: 'color', type: 'String' }
+      ]);
+
+      var testBuilder = TestBuilder(struct, { color: '"red"' });
+
+      expect(testBuilder.toString()).toContain('func withColor(color: String) -> AStructTestBuilder {');
+      expect(testBuilder.toString()).toContain('self.color = color');
+      expect(testBuilder.toString()).toContain('return self');
+    });
+
+    it('should include a build method', function() {
+      var struct = Struct();
+      struct.setName('AStruct');
+      struct.setProperties([
+        { name: 'color', type: 'String' }
+      ]);
+
+      var testBuilder = TestBuilder(struct, { color: '"red"' });
+
+      expect(testBuilder.toString()).toContain('func build() -> AStruct {');
+      expect(testBuilder.toString()).toContain('return AStruct(color: color)');
+    });
   });
 });
